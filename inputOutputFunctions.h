b0e81910f7b469053be3c69099b12d2ca039f223
MiLTaH <114093122+MiLTaH@users.noreply.github.com>
2022-12-04 20:00:51 +0300

    Create inputOutputFunctions.h

---
+++ inputOutputFunctions.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "iostream"
+#include "string"
+void input(std::string&);
+long double toDouble(std::string );
+std::string findNumbers(std::string, size_t, bool);
+template <class T>
+T power(T, size_t);
