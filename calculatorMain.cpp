170746d9a9961d451612e4f54a0d06a74e2d1bde
MiLTaH <114093122+MiLTaH@users.noreply.github.com>
2022-12-12 22:11:33 +0300

    Update calculatorMain.cpp

--- calculatorMain.cpp
+++ calculatorMain.cpp
@@ -1,5 +1,11 @@
 #include "inputOutputFunctions.h"
 int main() {
-	
+	try {
+		std::string expression;
+		input(expression);
+		calculate(expression);
+		std::cout << expression;
+	}
+	catch (const char* error) { std::cout << error; }
 	return 0;
 }
