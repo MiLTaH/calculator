bc0863dbab1056e980b1146c0f112762c4d0f5e6
MiLTaH <114093122+MiLTaH@users.noreply.github.com>
2022-12-12 22:14:06 +0300

    Create arithmeticAndLogicOperators.h

---
+++ arithmeticAndLogicOperators.h
@@ -0,0 +1,8 @@
+#pragma once 
+bool isInt(long double);
+bool isZero(long double);
+long double summary(long double, long double);
+long double subtract(long double, long double);
+long double multiplication(long double, long double);
+long double division(long double, long double);
+long double divisionWithRemainder(long double, long double);
