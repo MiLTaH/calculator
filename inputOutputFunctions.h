ae9e6c8986834dc92a621d64282aab5c2145facd
MiLTaH <114093122+MiLTaH@users.noreply.github.com>
2022-12-12 22:13:09 +0300

    Update inputOutputFunctions.h

--- inputOutputFunctions.h
+++ inputOutputFunctions.h
@@ -1,8 +1,14 @@
 #pragma once
 #include "iostream"
 #include "string"
+#include "vector"
+#include "arithmeticAndLogicOperators.h"
 void input(std::string&);
 long double toDouble(std::string );
 std::string findNumbers(std::string, size_t, bool);
 template <class T>
 T power(T, size_t);
+void binaryOperation(std::string&, std::string, long double (*) (long double, long double));
+void binaryOperationWithPriorities(std::string&, std::vector<std::string>,
+	std::vector<long double (*) (long double, long double)>);
+void calculate(std::string&);
