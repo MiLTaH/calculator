488ff02c8905f48e9cf4a27b711ebb5fb02e85d0
MiLTaH <114093122+MiLTaH@users.noreply.github.com>
2022-12-27 23:25:31 +0300

    Update arithmeticAndLogicOperators.h

--- arithmeticAndLogicOperators.h
+++ arithmeticAndLogicOperators.h
@@ -1,8 +1,19 @@
 #pragma once 
 bool isInt(long double);
 bool isZero(long double);
+bool isLogic(long double);
 long double summary(long double, long double);
 long double subtract(long double, long double);
 long double multiplication(long double, long double);
 long double division(long double, long double);
 long double divisionWithRemainder(long double, long double);
+long double root(long double);
+long double doublePower(long double, long double);
+long double doubleOr(long double, long double);
+long double doubleAnd(long double, long double);
+long double doubleXor(long double, long double);
+long double doubleNand(long double, long double);
+long double doubleNor(long double, long double);
+long double doubleSin(long double);
+long double doubleCos(long double);
+long double doubleTan(long double);
