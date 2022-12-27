4c72edf081e1220a6c667869f5388f3155a0a6fb
MiLTaH <114093122+MiLTaH@users.noreply.github.com>
2022-12-27 23:23:43 +0300

    Update inputOutputFunctions.h

--- inputOutputFunctions.h
+++ inputOutputFunctions.h
@@ -6,9 +6,15 @@
 void input(std::string&);
 long double toDouble(std::string );
 std::string findNumbers(std::string, size_t, bool);
-template <class T>
-T power(T, size_t);
-void binaryOperation(std::string&, std::string, long double (*) (long double, long double));
+std::string stringCopy(std::string, size_t, size_t);
+void UnaryOperation(std::string&, std::string, long double (*) (long double),bool);
+void binaryOperation(std::string&, std::string, long double (*) (long double,long double));
 void binaryOperationWithPriorities(std::string&, std::vector<std::string>,
 	std::vector<long double (*) (long double, long double)>);
+void brackets(std::string&, std::string, std::string);
 void calculate(std::string&);
+void UnaryPlus(std::string&);
+void checkRounding(std::string&);
+void doublePi(std::string&);
+template <class T>
+T power(T, size_t);
